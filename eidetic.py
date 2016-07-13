a434bcaa5743660c1d1df558428dd56c80877fa1
bennyty <bennytytyty@gmail.com>
2016-07-12 23:19:27 -0400

    Add rudementary return of URL, needs work.

--- eidetic.py
+++ eidetic.py
@@ -14,11 +14,23 @@ def parseInfoFile(infoFilename):
 		return lines[0], lines[1], lines[2], lines[3], lines[4:]
 
 
-def searchSite(baseUrl, searchUrl, whiteSpaceSeperator, finalRegex, regexs, searchTerm):
+def searchSite(baseUrl, searchUrl, whiteSpaceSeperator, finalRegex, regexes, searchTerm):
 	concatURL = (baseUrl + searchUrl + (''.join(str(n) for n in searchTerm)).replace(" ", whiteSpaceSeperator)).replace("\r", "").replace("\n", "")
 	html = getHTMLFromURL(concatURL).text
+
+	currentRegex = 0
+	# Loop until the finalRegex does match
+	while len( searchStringForRegex(html, finalRegex, False) ) == 0:
+		print("Did not match final regex\n\tnow matching " + regexes[currentRegex % len(regexes)])
+		capturesInOrder = searchStringForRegex(html, regexes[currentRegex % len(regexes)], True)
+		userChoice = userInteractionLayer(capturesInOrder[:10])
+		url = capturesInOrder[int(userChoice)][0] # Get the url from first element
+		html = getHTMLFromURL(concatURL).text
+		currentRegex += 1
 	
-	return concatURL
+
+	#This returns a located page url
+	return searchStringForRegex(html, finalRegex)
 
 def getUrlData(url, filename, bufferSize = 4096):
 	"""
@@ -42,12 +54,15 @@ def getUrlData(url, filename, bufferSize = 4096):
 
 def userInteractionLayer(data):
 	print("We found these results:")
+	print(data)
 	arrs = []
 	for x in range(0, len(data)):
 		element = list(data[x])
 		element.insert(0, str(x+1))
 		arrs.append(element)
-	col_width = max(len(word) for row in data for word in row) + 2
+	# col_width = max(len(word) for row in data for word in row) + 2
+	print(arrs)
+	col_width = max(len(word) for row in arrs for word in row) + 2
 	for row in arrs:
 		print("".join(word.ljust(col_width) for word in row))
 
@@ -58,14 +73,23 @@ def userInteractionLayer(data):
 def getHTMLFromURL(url):
 	return get(url)
 
-def searchStringForRegex(html, regex):
+def searchStringForRegex(html, regex, useIter):
 	"""
 	Return a tuple of all **capture groups**
-	>>> searchHTMLForRegex("Hello this is all", '\w*ll\w*')
+	>>> searchStringForRegex("Hello this is all", '\w*ll\w*')
 	['Hello', 'all']
 	"""
-	match = re.findall(regex, html)
-	return match
+	if useIter:
+		itr = re.finditer(regex, html)
+		arr = []
+		for match in itr:
+			element = []
+			element.append(match.group('url'))
+			element.append(match.group('name'))
+			arr.append(element)
+		return arr
+	else:
+		return re.findall(regex, html)
 
 def printUsageInstructions():
 	print("""\
